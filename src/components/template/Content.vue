179a14d377e67396767944b88becd3976ada0b1f
alanfxx <j.alands2@gmail.com>
2020-04-30 19:49:38 -0300

    layout inicial

---
+++ src/components/template/Content.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="app-content">
+        
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.app-content {
+    z-index: 1;
+}
+</style>
---
+++ src/components/template/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="app-footer">
+        <span>&copy; 2020 Alanfx</span>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.app-footer {
+    grid-area: 'footer';
+    border-top: 2px solid #1a6eff4f;
+    font-size: 0.75rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
